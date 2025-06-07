62f68e3e9dad60ea7a6da6bad3d37341a6cd0f38
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-07 12:02:49 -0400

    Add UserProvisioningFilter and UserRepository for user authentication handling

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/filters/UserProvisioningFilter.java
@@ -0,0 +1,37 @@
+package me.nayanm.tickets.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.repositories.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class UserProvisioningFilter extends OncePerRequestFilter<> {
+
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && authentication.getPrincipal() instanceof Jwt) {
+        }
+
+    }
+}
