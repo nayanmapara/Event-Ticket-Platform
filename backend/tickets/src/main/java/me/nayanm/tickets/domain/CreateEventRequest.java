d0702be347b2680d0643e91bde8561ec578fc072
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-27 10:44:36 -0400

    feat: Add CreateEventRequest class for event creation with necessary fields

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/domain/CreateEventRequest.java
@@ -0,0 +1,26 @@
+package me.nayanm.tickets.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.nayanm.tickets.domain.entities.EventStatusEnum;
+import me.nayanm.tickets.domain.entities.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequest {
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<CreateTicketTypeRequest> ticketTypes = new ArrayList<>();
+
+}
