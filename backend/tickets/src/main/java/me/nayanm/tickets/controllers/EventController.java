359cb74dfea07ca02d6c26a1f79d733e020dc0a8
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-29 02:32:04 -0400

    feat: Implement EventController for event creation endpoint

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/controllers/EventController.java
@@ -0,0 +1,42 @@
+package me.nayanm.tickets.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.domain.CreateEventRequest;
+import me.nayanm.tickets.domain.dtos.CreateEventRequestDto;
+import me.nayanm.tickets.domain.dtos.CreateEventResponseDto;
+import me.nayanm.tickets.domain.entities.Event;
+import me.nayanm.tickets.mappers.EventMapper;
+import me.nayanm.tickets.services.EventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventMapper eventMapper;
+    private final EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<CreateEventResponseDto> createEvent(
+            @AuthenticationPrincipal Jwt jwt,
+            @Valid @RequestBody CreateEventRequestDto createEventRequestDto) {
+
+        CreateEventRequest createEventRequest = eventMapper.fromDto(createEventRequestDto);
+        UUID userId = UUID.fromString(jwt.getSubject());
+        Event createdEvent = eventService.createEvent(userId, createEventRequest);
+        CreateEventResponseDto createEventResponseDto = eventMapper.toDto(createdEvent);
+
+        return new ResponseEntity<>(createEventResponseDto, HttpStatus.CREATED);
+    }
+}
