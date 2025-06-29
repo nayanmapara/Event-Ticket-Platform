007c37ad7fb967aa3cdfc589903fdc2447379e01
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-29 01:05:18 -0400

    feat: Create EventRepository interface for event data access

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/repositories/EventRepository.java
@@ -0,0 +1,11 @@
+package me.nayanm.tickets.repositories;
+
+import me.nayanm.tickets.domain.entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
