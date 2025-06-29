1e4267d7feb63b07ca9cf69600ae6dbbe94d0ff7
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-06-29 01:05:28 -0400

    feat: Add CreateTicketTypeRequestDto for ticket type creation with validation

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/domain/dtos/CreateTicketTypeRequestDto.java
@@ -0,0 +1,26 @@
+package me.nayanm.tickets.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTicketTypeRequestDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @PositiveOrZero(message = "Price must be greater than or equal to zero")
+    private Double price;
+
+    private String description;
+
+    private Integer totalAvailable;
+
+}
