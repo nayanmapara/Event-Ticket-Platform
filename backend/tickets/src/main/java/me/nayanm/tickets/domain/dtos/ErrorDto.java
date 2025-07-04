e1e96448b2923855e03d93e0d422849c74e1f19c
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-04 06:07:04 -0400

    feat: Add ErrorDto for standardized error responses

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/domain/dtos/ErrorDto.java
@@ -0,0 +1,12 @@
+package me.nayanm.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String error;
+}
