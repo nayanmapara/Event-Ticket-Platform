421f01c1f58710a1c2ff210f7032974db98a7724
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-08 10:00:06 -0400

    feat: Add updateEventForOrganizer method to EventService interface for event updates

--- backend/tickets/src/main/java/me/nayanm/tickets/services/EventService.java
+++ backend/tickets/src/main/java/me/nayanm/tickets/services/EventService.java
@@ -1,6 +1,7 @@
 package me.nayanm.tickets.services;
 
 import me.nayanm.tickets.domain.CreateEventRequest;
+import me.nayanm.tickets.domain.UpdateEventRequest;
 import me.nayanm.tickets.domain.entities.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,4 +13,5 @@ public interface EventService {
     Event createEvent(UUID organizerId, CreateEventRequest event);
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID eventId);
+    Event updateEventForOrganizer(UUID organizerId, UUID eventId, UpdateEventRequest event);
 }
