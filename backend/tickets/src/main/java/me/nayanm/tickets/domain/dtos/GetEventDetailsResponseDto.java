493a3672d4ba3c484ce681590337d97450d7a63a
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-08 06:42:33 -0400

    feat: Add GetEventDetailsResponseDto for detailed event response handling

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/domain/dtos/GetEventDetailsResponseDto.java
@@ -0,0 +1,28 @@
+package me.nayanm.tickets.domain.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.nayanm.tickets.domain.entities.EventStatusEnum;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetEventDetailsResponseDto {
+    private UUID id;
+    private String name;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private String venue;
+    private LocalDateTime salesStart;
+    private LocalDateTime salesEnd;
+    private EventStatusEnum status;
+    private List<GetEventTicketTypesResponseDto> ticketTypes = new ArrayList<>();
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
