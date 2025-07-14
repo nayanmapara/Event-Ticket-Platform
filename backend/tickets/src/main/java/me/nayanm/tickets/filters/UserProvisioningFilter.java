818e855f7fbf9a5fabd69693376b59b9bd907daa
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-14 07:37:02 -0400

    fix: Remove unnecessary blank line for improved code readability in UserProvisioningFilter

--- backend/tickets/src/main/java/me/nayanm/tickets/filters/UserProvisioningFilter.java
+++ backend/tickets/src/main/java/me/nayanm/tickets/filters/UserProvisioningFilter.java
@@ -44,7 +44,7 @@ public class UserProvisioningFilter extends OncePerRequestFilter {
                 userRepository.save(user);
             }
         }
-        
+
         filterChain.doFilter(request, response);
     }
 
