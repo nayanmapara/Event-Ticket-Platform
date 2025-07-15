c5257ec472c62882978a4799c0f665f5a52eb418
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-15 12:13:59 -0400

    feat: Implement TicketTypeServiceImpl for ticket purchasing functionality

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/services/impl/TicketTypeServiceImpl.java
@@ -0,0 +1,59 @@
+package me.nayanm.tickets.services.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.domain.entities.Ticket;
+import me.nayanm.tickets.domain.entities.TicketStatusEnum;
+import me.nayanm.tickets.domain.entities.TicketType;
+import me.nayanm.tickets.domain.entities.User;
+import me.nayanm.tickets.exceptions.TicketTypeNotFoundException;
+import me.nayanm.tickets.exceptions.TicketsSoldOutException;
+import me.nayanm.tickets.exceptions.UserNotFoundException;
+import me.nayanm.tickets.repositories.TicketRepository;
+import me.nayanm.tickets.repositories.TicketTypeRepository;
+import me.nayanm.tickets.repositories.UserRepository;
+import me.nayanm.tickets.services.QrCodeService;
+import me.nayanm.tickets.services.TicketTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TicketTypeServiceImpl implements TicketTypeService {
+
+    private final UserRepository userRepository;
+    private final TicketTypeRepository ticketTypeRepository;
+    private final TicketRepository ticketRepository;
+    private final QrCodeService qrCodeService;
+
+    @Override
+    @Transactional
+    public Ticket purchaseTicket(UUID userId, UUID ticketTypeId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(
+                String.format("User with id %s not found", userId)
+        ));
+
+        TicketType ticketType = ticketTypeRepository.findByIdWithLock(ticketTypeId).orElseThrow(() -> new TicketTypeNotFoundException(
+                String.format("Ticket type with id %s not found", ticketTypeId)
+        ));
+
+        int purchasedTickets = ticketRepository.countByTicketTypeId(ticketTypeId);
+        Integer totalAvailable = ticketType.getTotalAvailable();
+
+        if(purchasedTickets + 1 > totalAvailable) {
+            throw new TicketsSoldOutException();
+        }
+
+        Ticket ticket = new Ticket();
+        ticket.setStatus(TicketStatusEnum.PURCHASED);
+        ticket.setTicketType(ticketType);
+        ticket.setPurchaser(user);
+
+        Ticket savedTicket = ticketRepository.save(ticket);
+        qrCodeService.generateQrCode(savedTicket);
+
+        return ticketRepository.save(savedTicket);
+    }
+
+}
