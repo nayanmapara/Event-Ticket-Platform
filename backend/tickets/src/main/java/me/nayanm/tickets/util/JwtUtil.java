9f10d5b037899da62248141d63154c5be6688f1e
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-15 12:43:22 -0400

    feat: Move parseUserId method to JwtUtil for better code organization

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/util/JwtUtil.java
@@ -0,0 +1,14 @@
+package me.nayanm.tickets.util;
+
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.UUID;
+
+public final class JwtUtil {
+    private JwtUtil(){
+    }
+
+    public static UUID parseUserId(Jwt jwt) {
+        return UUID.fromString(jwt.getSubject());
+    }
+}
