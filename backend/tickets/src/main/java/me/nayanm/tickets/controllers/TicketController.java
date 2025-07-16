719bed2573691845ff16e3f6b661baa7467a5ef4
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-16 07:35:31 -0400

    feat: Add TicketController for managing ticket listings

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/controllers/TicketController.java
@@ -0,0 +1,39 @@
+package me.nayanm.tickets.controllers;
+
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.domain.dtos.ListTicketResponseDto;
+import me.nayanm.tickets.mappers.TicketMapper;
+import me.nayanm.tickets.services.TicketService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+import static me.nayanm.tickets.util.JwtUtil.parseUserId;
+
+@RestController
+@RequestMapping("/api/v1/tickets")
+@RequiredArgsConstructor
+public class TicketController {
+
+    private final TicketService ticketService;
+    private final TicketMapper ticketMapper;
+
+    @GetMapping
+    public Page<ListTicketResponseDto> listTickets(
+            @AuthenticationPrincipal Jwt jwt,
+            Pageable pageable
+    ) {
+        return ticketService.listTicketsForUser(
+                parseUserId(jwt),
+                pageable
+        ).map(ticketMapper::toListTicketResponseDto);
+    }
+
+
+}
