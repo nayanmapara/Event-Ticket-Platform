e150091c9756a213c6082f650c9d5fe7e25f5498
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-16 07:35:26 -0400

    feat: Implement TicketService and TicketServiceImpl for ticket management

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/services/impl/TicketServiceImpl.java
@@ -0,0 +1,30 @@
+package me.nayanm.tickets.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.domain.entities.Ticket;
+import me.nayanm.tickets.repositories.TicketRepository;
+import me.nayanm.tickets.services.TicketService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class TicketServiceImpl implements TicketService {
+
+    private final TicketRepository ticketRepository;
+
+    @Override
+    public Page<Ticket> listTicketsForUser(UUID userId, Pageable pageable) {
+        return ticketRepository.findByPurchaserId(userId, pageable);
+    }
+
+    @Override
+    public Optional<Ticket> getTicketForUser(UUID userId, UUID ticketId) {
+        return ticketRepository.findByIdAndPurchaserId(ticketId, userId);
+    }
+
+}
