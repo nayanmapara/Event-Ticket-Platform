eb38ebb4bc36b9da26f501f5bba880ca8b4934a3
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-17 01:28:36 -0400

    feat: Implement TicketValidationService and TicketValidationServiceImpl for ticket validation logic

---
+++ backend/tickets/src/main/java/me/nayanm/tickets/services/TicketValidationService.java
@@ -0,0 +1,10 @@
+package me.nayanm.tickets.services;
+
+import me.nayanm.tickets.domain.entities.TicketValidation;
+
+import java.util.UUID;
+
+public interface TicketValidationService {
+    TicketValidation validateTicketByQrCode(UUID qrCodeId);
+    TicketValidation validateTicketManually(UUID ticketId);
+}
---
+++ backend/tickets/src/main/java/me/nayanm/tickets/services/impl/TicketValidationServiceImpl.java
@@ -0,0 +1,59 @@
+package me.nayanm.tickets.services.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.nayanm.tickets.domain.entities.*;
+import me.nayanm.tickets.exceptions.QrCodeNotFoundException;
+import me.nayanm.tickets.exceptions.TicketNotFoundException;
+import me.nayanm.tickets.repositories.QrCodeRepository;
+import me.nayanm.tickets.repositories.TicketRepository;
+import me.nayanm.tickets.repositories.TicketValidationRepository;
+import me.nayanm.tickets.services.TicketValidationService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TicketValidationServiceImpl implements TicketValidationService {
+
+    private final QrCodeRepository qrCodeRepository;
+    private final TicketRepository ticketRepository;
+    private final TicketValidationRepository ticketValidationRepository;
+
+    @Override
+    public TicketValidation validateTicketByQrCode(UUID qrCodeId) {
+        QrCode qrCode = qrCodeRepository.findByIdAndStatus(qrCodeId, QrCodeStatusEnum.ACTIVE)
+                .orElseThrow(() -> new QrCodeNotFoundException(
+                        String.format("QR Code with id '%s' not found", qrCodeId)
+                ));
+
+        Ticket ticket = qrCode.getTicket();
+
+        return validateTicket(ticket);
+    }
+
+    private TicketValidation validateTicket(Ticket ticket) {
+        TicketValidation ticketValidation = new TicketValidation();
+        ticketValidation.setTicket(ticket);
+        ticketValidation.setValidationMethod(TicketValidationMethod.QR_SCAN);
+
+        TicketValidationStatusEnum ticketValidationStatus = ticket.getValidations().stream()
+                .filter(v -> TicketValidationStatusEnum.VALID.equals(v.getStatus()))
+                .findFirst()
+                .map(v -> TicketValidationStatusEnum.INVALID)
+                .orElse(TicketValidationStatusEnum.VALID);
+
+        ticketValidation.setStatus(ticketValidationStatus);
+
+        return ticketValidationRepository.save(ticketValidation);
+    }
+
+    @Override
+    public TicketValidation validateTicketManually(UUID ticketId) {
+        Ticket ticket = ticketRepository.findById(ticketId).orElseThrow(TicketNotFoundException::new);
+
+        return validateTicket(ticket);
+    }
+}
