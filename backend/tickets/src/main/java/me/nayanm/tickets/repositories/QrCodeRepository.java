c22d55bdf6221e3fb7f6861106217a1f43b4b992
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-17 00:48:23 -0400

    feat: Add method to find QrCode by ticketId and ticketPurchaserId

--- backend/tickets/src/main/java/me/nayanm/tickets/repositories/QrCodeRepository.java
+++ backend/tickets/src/main/java/me/nayanm/tickets/repositories/QrCodeRepository.java
@@ -4,9 +4,11 @@ import me.nayanm.tickets.domain.entities.QrCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
-
+    Optional<QrCode> findByTicketIdAndTicketPurchaserId(UUID ticketId, UUID ticketPurchaseId);
 }
