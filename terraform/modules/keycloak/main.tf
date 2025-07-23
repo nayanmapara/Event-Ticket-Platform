c1bf434fd3e80364588e942c1c673d3b1421a8ca
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2025-07-23 03:41:06 -0400

    fix: update keycloak app configuration to set always_on to false

--- terraform/modules/keycloak/main.tf
+++ terraform/modules/keycloak/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
-    always_on = true
+    always_on = false
   }
 
   app_settings = {
