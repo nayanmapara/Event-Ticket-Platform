db2015b3e25375c5a46a58fdc8e0da2db83ad812
Nayan Mapara <96513736+nayanmapara@users.noreply.github.com>
2026-02-22 19:17:13 -0500

    feat: update Azure deployment workflows for frontend and backend applications

--- terraform/modules/keycloak/main.tf
+++ terraform/modules/keycloak/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "plan" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "app" {
